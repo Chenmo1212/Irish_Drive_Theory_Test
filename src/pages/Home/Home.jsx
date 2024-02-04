b280b4097306e2e6fbea9b05e53e6660265a51f1
chenmo <chenmo1212@qq.com>
2024-02-04 10:22:21 +0000

    feat: Added progress

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,12 +4,12 @@ import {QUESTIONS_EN} from "../../data/questions_data";
 import DRIVER from '../../assets/svg/driver.svg'
 import {Link} from "react-router-dom";
 import {
-  loadFromLocalStorage,
-  saveToLocalStorage,
-  updateDataIfNewVersion,
-  THEME_COLOR,
   DEFAULT_VERSION,
-  NEW_VERSION
+  loadFromLocalStorage,
+  NEW_VERSION,
+  saveToLocalStorage,
+  THEME_COLOR,
+  updateDataIfNewVersion
 } from '../../common/common';
 
 const initializeLocalStorage = () => {
@@ -18,6 +18,7 @@ const initializeLocalStorage = () => {
   const isCN = loadFromLocalStorage('isCN', false);
   const isUpdate = updateDataIfNewVersion(currentVersion, NEW_VERSION);
   let allQuestions = loadFromLocalStorage('allQuestions', QUESTIONS_EN);
+  const allAnswers = loadFromLocalStorage('allAnswers', Array.from({length: allQuestions.length}, () => -1));
 
   if (isUpdate) {
     allQuestions = QUESTIONS_EN;
@@ -25,21 +26,27 @@ const initializeLocalStorage = () => {
     console.info(`App Updated: ${currentVersion} => ${NEW_VERSION}`)
   }
 
-  return {isCN, allQuestions, currQuestionIdx};
+  return {isCN, allQuestions, allAnswers, currQuestionIdx};
 };
 
 const Home = () => {
   const [allQuestions, setAllQuestions] = useState([]);
+  const [allAnswers, setAllAnswers] = useState([]);
   const [currQuestionIdx, setCurrQuestionIdx] = useState(1);
   const [isCN, setIsCN] = useState(false);
 
   useEffect(() => {
-    const {isCN, allQuestions, currQuestionIdx} = initializeLocalStorage();
+    const {isCN, allQuestions, allAnswers, currQuestionIdx} = initializeLocalStorage();
     setIsCN(isCN);
     setAllQuestions(allQuestions);
-    setAllQuestions(allQuestions);
+    setAllAnswers(allAnswers);
     setCurrQuestionIdx(currQuestionIdx);
   }, []);
+
+  const getProgressWidth = () => {
+    let res = allAnswers.filter(num => num !== -1).length / allAnswers.length;
+    return (res === 0 ? 0 : (res <= 3 ? 3 : res)) * 0.97 + "%";
+  }
 
   return <div className="home">
     <div className="project">
@@ -49,11 +56,15 @@ const Home = () => {
         </div>
         <div className="title">{isCN ? "爱尔兰驾照理论测试题" : "Irish Theory Test"}</div>
         <div className="question_type">
-          <span> {isCN ? "题目总数:" : "Question Amount:"} <span className="text-blue">{allQuestions.length}</span></span>
+          <span> {isCN ? "题目总数:" : "Question Amount:"} <span
+            className="text-blue">{allQuestions.length}</span></span>
         </div>
         <div className="progress">
           <div className="card-progress__back"></div>
-          <div className="card-progress__line" style={{width: '2%', backgroundColor: THEME_COLOR}}></div>
+          <div className="card-progress__line" style={{
+            width: getProgressWidth(),
+            backgroundColor: THEME_COLOR
+          }}></div>
         </div>
 
         <Link to={`/question/${currQuestionIdx}`}>
